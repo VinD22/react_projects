7ba46d4b08a717e32df11d83284ac21222bd164c
vinayak <vinayakdhadda2417@gmail.com>
2020-04-10 13:00:47 +0530

    React project with grid showcasing, using flex layout within the grid

---
+++ css-grids-example/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+// Logo Header 
+// Navbar Content 
+// Footer 
+
+function App() {
+  return (
+    <div className="container">
+      <div className="logo">
+        <div className="items">Logo</div>
+        <div className="items">Business Name</div>
+        <div className="items">2020</div>
+      </div>
+      <div className="header">Header</div>
+      <div className="nav">Navbar</div>
+      <div className="content">Content</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
+}
+
+export default App;
