57945a6afde0995e5ebebc26acc5360389c73f52
vinayak <vinayakdhadda2417@gmail.com>
2020-04-13 10:58:26 +0530

    flashcard-quiz-app with fetching data and laying out items, used react hooks useRef

--- flashcard-quiz-app/src/App.js
+++ flashcard-quiz-app/src/App.js
@@ -1,34 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from "./FlashcardList";
 import "./app.css";
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "What is 2 + 2?",
-    answer: "4",
-    options: [
-      '1', '2', '3', '4'
-    ]
-  },
-  {
-    id: 2,
-    question: "What is 2 + 1?",
-    answer: "3",
-    options: [
-      '1', '2', '3', '4'
-    ]
-  }
-]
+import axios from "axios";
 
 function App() {
 
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([])
+
+  const categoryEl = useRef();
+  const amountEl = useRef();
+
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api_category.php")
+      .then(res => {
+        setCategories(res.data.trivia_categories)
+      })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    axios
+      .get("https://opentdb.com/api.php", {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value
+        }
+      })
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [...questionItem.incorrect_answers.map(
+            a => decodeString(a)
+          ), answer]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5)
+          }
+        }))
+      })
+  }
 
   return (
-    <div>
-      <FlashcardList flashcards={flashcards} />
-    </div>
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category" >Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => (
+              <option value={category.id} id={category.id}>{category.name}</option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount" >Number of questions</label>
+          <input type="number" id="amount" min="1" step="1" defaultvalue={10} ref={amountEl} />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
   );
 }
 
