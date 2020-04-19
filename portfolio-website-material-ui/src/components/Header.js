477e85cfa7d67fe9e49ae1c70edc8838f6e5908c
vinayak <vinayakdhadda2417@gmail.com>
2020-04-19 20:31:09 +0530

    added animation with react particles library

--- portfolio-website-material-ui/src/components/Header.js
+++ portfolio-website-material-ui/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core/styles"
 import {
     Typography,
     Avatar,
@@ -8,20 +9,48 @@ import {
 import Typed from "react-typed";
 import avatar from "../avatar.png";
 
+// CSS Styles 
+const useStyles = makeStyles(theme => ({
+    avatar: {
+        width: theme.spacing(15),
+        height: theme.spacing(15),
+        margin: theme.spacing(1)
+    },
+    title: {
+        color: "tomato"
+    },
+    subtitle: {
+        color: "tan",
+        marginBototm: "3rem"
+    },
+    typedContainer: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "100vh",
+        textAlign: "center",
+        zIndex: 1
+    }
+}))
+
 
 const Header = () => {
+    const classes = useStyles()
     return (
-        <Box>
-            <Avatar src={avatar} alt="Russel Crowe" />
-            <Typography variant="h4">
+        <Box className={classes.typedContainer}>
+            <Grid container justify="center">
+                <Avatar src={avatar} alt="Russel Crowe" className={classes.avatar} />
+            </Grid>
+            <Typography variant="h4" className={classes.title}>
                 <Typed strings={["Vinayak"]} typeSpeed={40} />
             </Typography>
             <br />
-            <Typography variant="h5">
+            <Typography variant="h5" className={classes.subtitle}>
                 <Typed strings={["Web Developer", "Mobile App Developer", "UI Designer"]} typeSpeed={40} backSpeed={60} loop />
             </Typography>
         </Box>
     )
 }
 
-export default Header
+export default Header; 
