9eb7c9edb3e20af37e39d83d63656a3c0cb9b28f
vinayak <vinayakdhadda2417@gmail.com>
2020-05-18 11:09:38 +0530

    Added Charts and Daily Date

--- covid-with-material-ui/src/App.js
+++ covid-with-material-ui/src/App.js
@@ -5,6 +5,8 @@ import { fetchData } from './api'
 
 import styles from './App.module.css';
 
+import image from './covid.png';
+
 class App extends React.Component {
 
   state = {
@@ -14,21 +16,28 @@ class App extends React.Component {
   }
 
   async componentDidMount() {
-    const fetchedData = await fetchData();
-    this.setState({ data: fetchedData })
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
   }
 
   render() {
-
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
-        <Charts />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Charts data={data} country={country} />
       </div>
-    )
+    );
   }
 }
 
