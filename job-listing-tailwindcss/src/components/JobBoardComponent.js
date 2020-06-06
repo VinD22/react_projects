77b91804d78572e3178548b99d69575dc4b5bd21
vinayak <vinayakdhadda2417@gmail.com>
2020-06-06 12:22:47 +0530

    Finished UI for the list items

--- job-listing-tailwindcss/src/components/JobBoardComponent.js
+++ job-listing-tailwindcss/src/components/JobBoardComponent.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
 const JobBoardComponent = ({ job }) => {
+
+    const languagesAndTools = [...job.languages, ...job.tools];
+
+    console.log(languagesAndTools);
+
     return (
-        <div className="flex bg-white shadow-md m-4 p-4">
+        <div className="flex bg-white shadow-md m-4 p-6">
             <div>
                 <img src={job.logo} alt={job.company} />
             </div>
-            <div></div>
-            <div></div>
+            <div className="ml-4 flex flex-col justify-between">
+                <h3 className="font-bold  text-teal-500">{job.company}</h3>
+                <h2 className="font-bold text-lg">{job.position}</h2>
+                <p className="text-gray-700">{job.postedAt} · {job.contract} · {job.location}</p>
+            </div>
+            <div className="flex items-center ml-auto">
+                {languagesAndTools.map((lnt, index) => <span key={index} className="text-teal-500 bg-teal-100 font-bold m-1 p-2 rounded leading-3"> {lnt} </span>)}
+            </div>
         </div>
     )
 }
