112fa1007f522439c3ac2eca72aab399540730dc
vinayak <vinayakdhadda2417@gmail.com>
2020-06-20 17:37:15 +0530

    css card example

--- css-transitions/src/App.js
+++ css-transitions/src/App.js
@@ -6,10 +6,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hi
-      </header>
-      <p>lasd,as jasdlaskjd alksjdalskjd laskdjlka </p>
+      {/* <p>practicing css with before and after pseudo operators </p>
+      <br />
+      <br />
+      <br /> */}
+
+      <div className="box">
+        <img src="https://image.flaticon.com/icons/svg/138/138292.svg" alt="coins" />
+        <h2>donate</h2>
+        <p>Support us <br /> Donate Below</p>
+
+        <a href="#" target="_blank">
+          <i className="fab fa-paypal"></i>
+          Paypal
+        </a>
+
+        <a href="#" target="_blank">
+          <i className="fab fa-patreon"></i>
+          Patreon
+        </a>
+
+      </div>
+
+
+
     </div>
   );
 }
