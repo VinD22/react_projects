d51160822ec1fa4a82c46c0f3d25f4c8b923c53b
vinayak <vinayakdhadda2417@gmail.com>
2020-06-30 21:28:09 +0530

    react context practice

--- context-api-boilerplate/src/MovieListContext.js
+++ context-api-boilerplate/src/MovieListContext.js
@@ -5,21 +5,7 @@ export const MovieListContext = createContext();
 export const MovieProvider = (props) => {
 
     const [movies, setMovies] = useState([
-        {
-            name: 'Harry potter',
-            price: '$20',
-            id: 1
-        },
-        {
-            name: 'Extraction',
-            price: '$200',
-            id: 2
-        },
-        {
-            name: 'The last dance',
-            price: '$240',
-            id: 3
-        }
+
     ])
 
     return (
