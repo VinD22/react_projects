86a31bb12006fd8e295c05855fa439235cd51d51
vinayak <vinayakdhadda2417@gmail.com>
2020-07-07 21:50:17 +0530

    Data table app

---
+++ data-table-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h2>Data table app</h2>
+      <table>
+        <th>Header</th>
+        <tr>
+          Row 1
+          </tr>
+      </table>
+    </div >
+  );
+}
+
+export default App;
