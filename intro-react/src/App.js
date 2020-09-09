3f819d0db3efc14e859d2374dd0f756e1156683a
vinayak <vinayakdhadda2417@gmail.com>
2020-09-09 08:48:46 +0530

    states

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Documentation from "./Documentation";
 
 function App() {
 
+  const [count, setCount] = useState(0);
+
   const textToDisplay = "https://www.youtube.com/playlist?list=PLQg6GaokU5CyvExiaMgXP_BS5WWNBfZJN"
 
   return (
     <div className="App">
       {textToDisplay}
+        Things to learn : JSX, props, states, useEffect, external libraries.
       <Documentation />
+      <Button onClick={() => setCount(count + 1)} />
     </div>
   );
 }
